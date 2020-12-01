9786ba43736cbf4db9bebd3849cf5afef3d9b4bd
TheGuitarleader <guitarleaderbusiness@gmail.com>
2020-11-30 20:16:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const Twit = require('twit');
-const {
-    consumer_key,
-    consumer_secret,
-    access_token,
-    access_token_secret,
-    discord_prefix,
-    discord_token,
-    discord_channel,
-    donation_link
-} = require('./config.json');
+const config = require('./config.json');
 
 var logType = ['ERROR', 'CONN', 'INFO', 'DEBUG'];
 
@@ -21,10 +12,10 @@ const client = new Discord.Client();
 var EventEmitter = require('events');
 var Tweeter = new EventEmitter();
 var Twitter = new Twit({
-    consumer_key: consumer_key,
-    consumer_secret: consumer_secret,
-    access_token: access_token,
-    access_token_secret: access_token_secret
+    consumer_key: config.twitter.consumer_key,
+    consumer_secret: config.twitter.consumer_secret,
+    access_token: config.twitter.access_token,
+    access_token_secret: config.twitter.access_token_secret
 });
 
 // Discord
@@ -47,7 +38,7 @@ client.once('disconnect', () => {
 });
 
 client.on('message', message => {
-    if (message.content.startsWith(`${discord_prefix}ping`)) {
+    if (message.content.startsWith(`${config.discord.prefix}ping`)) {
         message.channel.send("Pinging...").then(msg =>{
               var botping = Math.round(client.ws.ping)
               var ping = msg.createdTimestamp - message.createdTimestamp;
@@ -61,42 +52,52 @@ client.on('message', message => {
         });
     }
 
-    if(message.content.startsWith(`${discord_prefix}list`)) {
+    if(message.content.startsWith(`${config.discord.prefix}list`)) {
         const embedMessage = new Discord.MessageEmbed()
         .setColor('#1DA1F2')
         .setTitle('Currently following these Twitter accounts')
-        .setDescription('- @PlayApex\n- @shrugtal\n- @MannyHagopian\n- @MonsterclipRSPN\n- @ChadGrenier\n- @zylbrad7\n- @monsterhunter\n- @tommiecas')
+        .setDescription('- @PlayApex\n- @shrugtal\n- @monsterhunter\n')
     
         message.channel.send(embedMessage);
     }
 
-    if(message.content.startsWith(`${discord_prefix}test`)) {
-        client.channels.cache.get(discord_channel).send(':white_check_mark: Your tweets will show up here!');
+    if(message.content.startsWith(`${config.discord.prefix}test`)) {
+        client.channels.cache.get(config.discord.channel).send(':white_check_mark: Your tweets will show up here!');
     }
 
-    if(message.content.startsWith(`${discord_prefix}disconnect`)) {
+    if(message.content.startsWith(`${config.discord.prefix}disconnect`)) {
         Tweeter.emit('stop');
     }
 
-    if(message.content.startsWith(`${discord_prefix}connect`)) {
+    if(message.content.startsWith(`${config.discord.prefix}connect`)) {
         Tweeter.emit('start');
     }
 
-    if(message.content.startsWith(`${discord_prefix}donate`)) {
+    if(message.content.startsWith(`${config.discord.prefix}stop`)) {
+        if(message.member.hasPermission('ADMINISTRATOR')) {
+            message.channel.send(":white_check_mark: Shutting down...").then(msg => {
+                process.exit();
+            });
+        } else {
+            message.channel.send(":rage: You can't kill me!");
+        }
+    }
+
+    if(message.content.startsWith(`${config.discord.prefix}donate`)) {
         const embedMessage = new Discord.MessageEmbed()
         .setColor('#1DA1F2')
-        .setAuthor('Donation Info', client.user.avatarURL(), donation_link)
+        .setAuthor('Donation Info', client.user.avatarURL(), config.discord.donation_link)
         .setDescription("Tweeter is just one of many projects I'm working on. If you love Tweeter please consider donating to help support the development of this" + 
         "bot and many more to come! Donating also helps invest in better hardware to run this bot.")
         message.channel.send(embedMessage);
     }
 });
 
-client.login(discord_token);
+client.login(config.discord.token);
 
 // Twitter
 Tweeter.on('start', function (save) {
-    let userIds = [1048018930785083392, 1038220641311309829, 438653616, 20950208, 41494136, 703828455683596288, 306490355, 15865245, 3774450793, 3190084598 ]
+    let userIds = config.twitter.followed_accounts
     var stream = Twitter.stream('statuses/filter', { follow: userIds });
 
     stream.on('connect', function (request) {
@@ -131,24 +132,10 @@ Tweeter.on('start', function (save) {
                 .setFooter('From Twitter')
             
                 consoleLog(logType[2], `Forwarding tweet from ${tweet.user.screen_name} to Discord ->`);
-                client.channels.cache.get(discord_channel).send(embedMessage);
+                client.channels.cache.get(config.discord.channel).send(embedMessage);
             }
         }
     });
-
-    Tweeter.on('stop', function (disconnect) {
-        consoleLog(logType[1], 'Stopped the data stream');
-        stream.stop();
-        client.channels.cache.get(discord_channel).send(':white_check_mark: Terminated connection to data stream');
-        client.user.setStatus('dnd');
-    })
-
-    Tweeter.on('start', function (disconnect) {
-        consoleLog(logType[1], 'Restarted the data stream');
-        sleep(1000);
-        stream.start();
-        client.channels.cache.get(discord_channel).send(':white_check_mark: Attempting connection to data stream');
-    })
 });
 
 function sleep(ms) {
